799f343e8592532d421d14c65f03aba5e5bddd31
ExtraYin <yyin44@wisc.edu>
2016-06-30 20:42:39 +0800

    finish week5

--- ML/Coursera-ML/machine-learning-ex4/ex4/nnCostFunction.m
+++ ML/Coursera-ML/machine-learning-ex4/ex4/nnCostFunction.m
@@ -62,20 +62,47 @@ Theta2_grad = zeros(size(Theta2));
 %               and Theta2_grad from Part 2.
 %
 
+X = [ones(m, 1) X];
+z2 = X * Theta1';
+a2 = sigmoid(z2);
+a2 = [ones(size(a2,1),1) a2];
+z3 = a2 * Theta2';
+a3 = sigmoid(z3);
+for i = 1:m
+    x_col = a3(i,:);
+    % ?y(i)log((h¦È(x(i)))k)?(1?y(i))log(1?(h¦È(x(i)))k)   
+    y_col = zeros(1,num_labels);
+    y_col(y(i)) = 1;
+    J = J - y_col * (log(x_col))' - (1-y_col) * (log(1-x_col))';
+end
+J = J/m;
+
+t1 = Theta1(:,2:size(Theta1,2));
+t2 = Theta2(:,2:size(Theta2,2));
+J = J + lambda/(2*m) * (sum(sum(t1.^2)) + sum(sum(t2.^2)));
 
 
+% calculate gradient
+for i = 1:m
+	a1 = X(i,:)';
+	z2 = Theta1 * a1;
+	a2 = [1; sigmoid(z2)];
+	z3 = Theta2 * a2;
+	a3 = sigmoid(z3);
 
+    y_col = zeros(1,num_labels);
+    y_col(y(i)) = 1;
+	delta_3 = a3 - y_col';
 
+	delta_2 = (Theta2' * delta_3) .* [1; sigmoidGradient(z2)];
+	delta_2 = delta_2(2:end);
 
+	Theta1_grad = Theta1_grad + delta_2 * a1';
+	Theta2_grad = Theta2_grad + delta_3 * a2';
+end
 
-
-
-
-
-
-
-
-
+Theta1_grad = (1/m) * Theta1_grad + (lambda/m) * [zeros(size(Theta1, 1), 1) Theta1(:,2:end)];
+Theta2_grad = (1/m) * Theta2_grad + (lambda/m) * [zeros(size(Theta2, 1), 1) Theta2(:,2:end)];
 
 
 
